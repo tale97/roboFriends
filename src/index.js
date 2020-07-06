cf633331f35b08fd5651023bf26d21ecb7678d75
tale97 <ttle@college.harvard.edu>
2020-07-05 21:10:58 -0400

    starting incorporate redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 import App from "./containers/App";
+import { searchRobots } from "./reducers.js";
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <div>
