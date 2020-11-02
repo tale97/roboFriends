a7131805078daec289128fbdc9b257553cea5f1b
tale97 <ttle@college.harvard.edu>
2020-11-01 20:06:24 -0500

    Updates

---
+++ precache-manifest.0a9c10045b741f19eb3e6c9326eb09fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf3a9db67f146b6e6512ad46c7bd7f2",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "a8bb5bcce81fd33461d4",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2dcbf4d98706d4186239",
+    "url": "/roboFriends/static/css/main.bec27629.chunk.css"
+  },
+  {
+    "revision": "a8bb5bcce81fd33461d4",
+    "url": "/roboFriends/static/js/2.198d9cf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roboFriends/static/js/2.198d9cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcbf4d98706d4186239",
+    "url": "/roboFriends/static/js/main.18b9570f.chunk.js"
+  },
+  {
+    "revision": "821fe69068fb5b686875",
+    "url": "/roboFriends/static/js/runtime-main.d8e71509.js"
+  }
+]);
